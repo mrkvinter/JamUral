e2e422e2e95441258eef8d6241ddb91cbfe709bf
DmitryGaranin <strah08i@gmail.com>
2016-04-04 14:07:21 +0500

    Add sample scene how to use EventManager

---
+++ JamProject/Assets/Scripts/ForSampleUseEventManager/AddScore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AddScore : MonoBehaviour {
+
+    public Text txtRef;
+
+    public int money = 400;
+
+    void Start()
+    {
+        EventManager.AddListener("AddMoney", AddMoney);
+        SetText(string.Format("Score: {0}", money));
+    }
+
+    void SetText(string text)
+    {
+        if (txtRef != null)
+            txtRef.text = text;
+    }
+
+    public void AddMoney()
+    {
+        money += 100;
+        SetText(string.Format("Score: {0}", money));
+        Debug.Log("Event AddScore was passed");
+    }
+}
---
+++ JamProject/Assets/Scripts/ForSampleUseEventManager/ClickButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickButton : MonoBehaviour {
+
+    public void OnClick()
+    {
+        Debug.Log("I was clicked and I make a gold");
+        EventManager.Invoke("AddMoney");
+    }
+}
