2e741433ce05c100eca6fef374c7a321aa173c1e
Danil <2yodi@mail.ru>
2016-04-04 22:17:29 +0500

    add start scene

---
+++ JamProject/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour
+{
+    public void LoadScene(int level)
+    {
+        //loadingImage.SetActive(true);
+        Application.LoadLevel(level);
+
+        //с LoadScene не работает :(
+        //LoadScene(level);
+    }
+
+}
